b92e1f9bf188bc839da223f361c96fc4fdff278a
shrirajpawar4 <pawarshriraj4@gmail.com>
2023-10-12 14:35:34 +0530

    uniswap v3 init

---
+++ test/ERC20Mintable.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.14;
+
+import "../lib/solmate/src/tokens/ERC20.sol";
+
+contract ERC20Mintable is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
