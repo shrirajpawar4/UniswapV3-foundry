c248f6429a23a3982d2875046fbc0f4811f174f2
shrirajpawar4 <pawarshriraj4@gmail.com>
2023-11-20 14:25:44 +0530

    feat: UniswapV3Pool contract

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -1,94 +1,10 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.14;
-
-// import "forge-std/Test.sol";
-// import './ERC20Mintable.sol';
-// import '../src/UniswapV3Pool.sol';
-
-
-// contract UniswapV3PoolTest is Test {
-//     ERC20Mintable token0;
-//     ERC20Mintable token1;
-//     UniswapV3Pool pool;
-
-
-//     function setUp() public {
-//         token0 = new ERC20Mintable("Ether", "ETH", 18);
-//         token1 = new ERC20Mintable("USDC", "USDC", 18);
-//     }
-
-//     function testMintSuccess() public {
-//     TestCaseParams memory params = TestCaseParams({
-//         wethBalance: 1 ether,
-//         usdcBalance: 5000 ether,
-//         currentTick: 85176,
-//         lowerTick: 84222,
-//         upperTick: 86129,
-//         liquidity: 1517882343751509868544,
-//         currentSqrtP: 5602277097478614198912276234240,
-//         shouldTransferInCallback: true,
-//         mintLiqudity: true
-//     });
-//     (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
-
-//     function testExample() public {
-//         assertTrue(true);
-//     }
-
-//     function uniswapV3MintCallback(uint256 amount0, uint256 amount1) public {
-//     if (shouldTransferInCallback) {
-//         token0.transfer(msg.sender, amount0);
-//         token1.transfer(msg.sender, amount1);
-//     }
-// }
-
-
-//     function setupTestCase(TestCaseParams memory params)
-//         internal
-//         returns (uint256 poolBalance0, uint256 poolBalance1)
-//     {
-//         token0.mint(address(this), params.wethBalance);
-//         token1.mint(address(this), params.usdcBalance);
-
-//         pool = new UniswapV3Pool(
-//             address(token0),
-//             address(token1),
-//             params.currentSqrtP,
-//             params.currentTick
-//         );
-
-//         if (params.mintLiqudity) {
-//             token0.approve(address(this), params.wethBalance);
-//             token1.approve(address(this), params.usdcBalance);
-
-//             UniswapV3Pool.CallbackData memory extra = UniswapV3Pool
-//                 .CallbackData({
-//                     token0: address(token0),
-//                     token1: address(token1),
-//                     payer: address(this)
-//                 });
-
-//             (poolBalance0, poolBalance1) = pool.mint(
-//                 address(this),
-//                 params.lowerTick,
-//                 params.upperTick,
-//                 params.liquidity,
-//                 abi.encode(extra)
-//             );
-//         }
-
-//         transferInMintCallback = params.transferInMintCallback;
-//         transferInSwapCallback = params.transferInSwapCallback;
-//     }
-// }
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
 import "./ERC20Mintable.sol";
 import "../src/UniswapV3Pool.sol";
-//import "./TestUtils.sol";
+import "./TestUtils.sol";
 
 contract UniswapV3PoolTest is Test, TestUtils {
     ERC20Mintable token0;
